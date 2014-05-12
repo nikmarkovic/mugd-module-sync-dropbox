9018a3f7b9ebef6a31aa3d42c53fc5c7f0e5eaa0
nikmarkovic <nik.markovic.91@gmail.com>
2014-05-12 22:40:18 +0200

    first commit

---
+++ Scripts/Core/Editor/Sync/Drive/Dropbox/DropboxDetection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Assets.Scripts.Core.Editor.Sync.Drive.Dropbox
+{
+    public class DropboxDetection : IDriveDetection
+    {
+        public string DriveName
+        {
+            get { return "Dropbox"; }
+        }
+
+#if UNITY_STANDALONE_WIN
+        public string DrivePath
+        {
+            get
+            {
+                var dbPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Dropbox\\host.db";
+                var lines = File.ReadAllLines(dbPath);
+                var dbBase64Text = Convert.FromBase64String(lines[1]);
+                return Encoding.ASCII.GetString(dbBase64Text);
+            }
+        }
+#endif
+
+#if UNITY_STANDALONE_OSX
+        public string DrivePath
+        {
+            get
+            { throw new NotImplementedException(); }
+        }
+#endif
+    }
+}
